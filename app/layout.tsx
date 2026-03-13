8e79cb34d2c753841b087cb29a79d47749c94585
thenielslima-dev <theniels.lima@gmail.com>
2026-03-13 19:30:45 -0300

    Mudança na tela inicial e na barra de guia do navegador v2

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "SANE Sistemas",
-  description: "Sistema profissional de orçamentos e gestão",
+  description: "Sistema profissional de orçamentos",
 };
 
 export default function RootLayout({
